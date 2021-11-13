207f3d2dc5b70d8528b342a4d2cfff4287c20621
Josh Ruihley <ruihley@gmail.com>
2021-11-13 08:07:24 -0500

    initial 256klabs code

---
+++ SSMP.c
@@ -0,0 +1,1 @@
+#include "SSMP.h"
---
+++ keymaps/via/keymap.c
@@ -0,0 +1,22 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[0] = LAYOUT_all(
+		KC_F1, KC_F2, KC_F3, KC_F4,
+		KC_F5, KC_F6, KC_F7, MO(1)),
+
+	[1] = LAYOUT_all(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+	[2] = LAYOUT_all(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+	[3] = LAYOUT_all(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+};
+
